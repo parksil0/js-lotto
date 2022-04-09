b760978145f3aadee97bf86ec113ac7a9b21c015
parksil0 <parksil00@naver.com>
2022-04-10 02:10:39 +0900

    feat : view 관련 조상 클래스 생성

---
+++ src/Views/View.js
@@ -0,0 +1,26 @@
+export default class View {
+  constructor(element) {
+    this.element = element;
+
+    this.originalDisplay = this.element.style.display || '';
+  }
+
+  hide() {
+    this.element.style.display = 'none';
+    return this;
+  }
+
+  show() {
+    this.element.style.display = this.originalDisplay;
+    return this;
+  }
+
+  emit(eventName, data) {
+    emit(this.element, eventName, data);
+    return this;
+  }
+
+  addEventListener(eventName, handler) {
+    this.element.addEventListener(eventName, handler);
+  }
+}
