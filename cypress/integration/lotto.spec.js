a0c9ffc3b7f47476c3e9b053ea3faefd24c1d946
parksil0 <parksil00@naver.com>
2022-04-10 01:43:44 +0900

    test: 로또 구입 금액이 1000원 미만인 경우, 에러 메세지를 호출 여부를 확인하는 테스트 케이스 추가

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -10,4 +10,14 @@ describe('step1 필수 요구사항', () => {
       expect(input[0].validationMessage).to.eq('Please fill out this field.');
     });
   });
+
+  it('로또 구입 금액이 1000원 미만인 경우, 에러메세지를 호출한다.', () => {
+    cy.get('#input-price').type(500);
+    cy.get('#input-price-btn').click();
+    cy.get('#input-price').then((input) => {
+      expect(input[0].validationMessage).to.eq(
+        'Value must be greater than or equal to 1000.',
+      );
+    });
+  });
 });
