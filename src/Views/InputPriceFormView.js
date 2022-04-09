15ec6f2dcc431cda193265d53a0b481a4a21859f
parksil0 <parksil00@naver.com>
2022-04-10 02:31:36 +0900

    feat : 구입 입력 폼 관련 view 생성
     - input 유효성검사 설정

--- src/Views/InputPriceFormView.js
+++ src/Views/InputPriceFormView.js
@@ -4,5 +4,28 @@ import View from './View.js';
 export default class InputPriceFormView extends View {
   constructor() {
     super($('#input-price-form'));
+
+    this.inputPrice = $('#input-price');
+    this.inputPriceButton = $('#input-price-btn');
+
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.inputPriceButton.addEventListener(
+      'click',
+      this.handleButtonClick.bind(this),
+    );
+  }
+
+  handleButtonClick(e) {
+    const { value } = this.inputPrice;
+
+    if (Number(value) >= 1000 && Number(value) % 1000 !== 0) {
+      e.preventDefault();
+      alert('로또 구입 금액을 1,000원 단위로 입력해 주세요.');
+      this.inputPrice.value = '';
+      return;
+    }
   }
 }
