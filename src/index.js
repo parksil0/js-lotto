e413fcb6dc35032b6814ea1f025eb463c09a69a3
parksil0 <parksil00@naver.com>
2022-04-10 02:11:52 +0900

    feat : lotto controller 설정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-const $showResultButton = document.querySelector('.open-result-modal-button');
-const $modalClose = document.querySelector('.modal-close');
-const $modal = document.querySelector('.modal');
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button',
-);
+import Controller from './Controller.js';
+import InputPriceFormView from './Views/InputPriceFormView.js';
 
-const onModalShow = () => {
-  $modal.classList.add('open');
-};
+const lottoController = new Controller({
+  inputPriceFormView: new InputPriceFormView(),
+});
 
-const onModalClose = () => {
-  $modal.classList.remove('open');
-};
-
-$showResultButton.addEventListener('click', onModalShow);
-$modalClose.addEventListener('click', onModalClose);
+lottoController.initialize();
