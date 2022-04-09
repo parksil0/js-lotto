4510907e607e134042eabfc865f6f3e6dadf01e3
parksil0 <parksil00@naver.com>
2022-04-10 02:11:16 +0900

    feat : 로또 controller 생성

---
+++ src/Controller.js
@@ -0,0 +1,13 @@
+export default class Controller {
+  constructor({ inputPriceFormView }) {
+    this.inputPriceFormView = inputPriceFormView;
+  }
+
+  initialize() {
+    this.render();
+  }
+
+  render() {
+    // TODO: view의 hide & show 분기처리
+  }
+}
