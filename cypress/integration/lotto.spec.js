b825b19d89a183e8a9dfd904f81c5b218134ad52
parksil0 <parksil00@naver.com>
2022-04-10 01:33:44 +0900

    test: 로또 구입 금액 input 필수 입력 확인 테스트

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -3,7 +3,11 @@ describe('step1 필수 요구사항', () => {
     cy.visit('http://127.0.0.1:5500');
   });
 
-  it('초기 테스트', () => {
-    //
+  it('로또 구입 금액 input은 필수 입력이다.', () => {
+    cy.get('#input-price-btn').click();
+    cy.get('input:invalid').should('have.length', 1);
+    cy.get('#input-price').then((input) => {
+      expect(input[0].validationMessage).to.eq('Please fill out this field.');
+    });
   });
 });
